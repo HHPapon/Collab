365de7b0aca986e7ae2cc45f939eb1a581bdb24c
Papon <mdhefzulhossainpapon.cse@diu.edu.bd>
2025-02-05 11:14:31 +0600

    New file created

---
+++ new.dart
@@ -0,0 +1,17 @@
+class Person {
+  String name;
+  int age;
+
+  // Main constructor
+  Person(this.name, this.age);
+
+  // Redirecting constructor
+  Person.newBorn(String name) : this(name, 0);
+}
+
+void main() {
+  Person person = Person.newBorn('Baby');
+  print(person.name); // Output: Baby
+  print(person.age);  // Output: 0
+}
+
